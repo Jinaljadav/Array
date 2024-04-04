b36d5e45ad7b0a59ab7da13d4e395db0a029f5f8
Jinaljadav <86101125+Jinaljadav@users.noreply.github.com>
2024-04-04 15:14:01 -0400

    Add files via upload

---
+++ aarays.java
@@ -0,0 +1,17 @@
+package com.example.array;
+
+public class aarays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String[] Fruits= {"Mango","Apple", "Orange", "Watermelon", "Pineapple", "Banana"};
+		
+				System.out.println("Fruit name is : " + Fruits[2]);
+				
+				System.out.println("Length of an array : "+ Fruits.length);
+		
+		
+	}
+
+}
